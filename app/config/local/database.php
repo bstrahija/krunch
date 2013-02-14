e1a2cadcd4bb392f26e34007852c5870db686e2b
Boris Strahija <boris@creolab.hr>
2013-02-14 17:30:39 +0100

    Dev env

---
+++ app/config/local/database.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'sqlite' => array(
+			'driver'   => 'sqlite',
+			'database' => __DIR__.'/../database/production.sqlite',
+			'prefix'   => '',
+		),
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'krunch',
+			'username'  => 'root',
+			'password'  => 'root',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+	),
+
+);
