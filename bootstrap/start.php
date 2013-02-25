d74f93f2fb3220728c142e7dbefc5b7507403dab
Boris Strahija <boris@creolab.hr>
2013-02-25 15:54:13 +0100

    Env setup

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('*.dev'),
+	'local' => array('*.dev', 'MBP.local'),
+	'production' => array('*.creolab.hr'),
 
 ));
 
